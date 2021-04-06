a9709e2b4bd939299faeaf88c647d6f396dcb268
stanislawdann <dannstanislaw@gmail.com>
2021-04-06 16:19:43 +0200

    first basic models and db

---
+++ articles/models.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Tytuł")
+    content = models.TextField(verbose_name="zawartość")
+    published = models.DateTimeField(verbose_name="Data publikacji")
+
+    def __unicode__(self):
+        return self.title
